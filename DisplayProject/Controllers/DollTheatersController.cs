a2ed926abb1f351a0d53c521dfd7debf097dab18
Andrew-star-code <rock.mothershipe@gmail.com>
2023-06-11 21:08:06 +0400

    Add project files.

---
+++ DisplayProject/Controllers/DollTheatersController.cs
@@ -0,0 +1,47 @@
+﻿using DisplayProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DisplayProject.Controllers
+{
+    public class DollTheatersController : Controller
+    {
+        private static List<DollTheaters> dollTheaters = new List<DollTheaters>()
+    {
+        new DollTheaters { Id = 1, Name = "Самарский театр кукол", City = "Самара", OpenDate = new DateTime(1932, 10, 20), NumberOfPerfomances = 102, AveragePerfomancePrice = 1350, AnnualRevenue = 2100000000 },
+        new DollTheaters { Id = 2, Name = "Санкт-Петербургский кукольный театр сказки", City = "Санкт-Петербург", OpenDate = new DateTime(1944, 2, 10), NumberOfPerfomances = 75, AveragePerfomancePrice = 1590, AnnualRevenue = 1700000000 },
+        new DollTheaters { Id = 3, Name = "Сахалинский театр кукол", City = "Сахалин", OpenDate = new DateTime(1985, 11, 24), NumberOfPerfomances = 11, AveragePerfomancePrice = 4400, AnnualRevenue = 600000000 }
+    };
+
+        private void AddDollTheaterToList(DollTheaters dollTheater)
+        {
+            dollTheater.Id = dollTheaters.Count + 1;
+            dollTheaters.Add(dollTheater);
+        }
+
+        public ActionResult Index()
+        {
+            return View(dollTheaters.ToList());
+        }
+
+        [HttpGet]
+        public ActionResult AddTheater()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddTheater(DollTheaters dollTheater)
+        {
+            if (ModelState.IsValid)
+            {
+                AddDollTheaterToList(dollTheater);
+                return RedirectToAction("Index");
+            }
+            return View(dollTheater);
+        }
+    }
+}
---
+++ DisplayProject/Models/DollTheaters.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace DisplayProject.Models
+{
+    public class DollTheaters
+    {
+        public int Id { get; set; }
+
+        [DisplayName("Название театра")]
+        public string Name { get; set; }
+
+        [DisplayName("Город")]
+        public string City { get; set; }
+
+        [DisplayName("Дата основания")]
+        [DataType(DataType.Date)]
+        public DateTime OpenDate { get; set; }
+
+        [DisplayName("Количество представлений")]
+        public int NumberOfPerfomances { get; set; }
+
+        [DisplayName("Средняя цена билета")]
+        public double AveragePerfomancePrice { get; set; }
+
+        [DisplayName("Ежегодный доход")]
+        public decimal AnnualRevenue { get; set; }
+    }
+}
